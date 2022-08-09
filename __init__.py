b6029a25541c2fbb11c293334de1e2efbc082ab1
Rhapsodos <wjddyd94@naver.com>
2022-08-10 00:20:09 +0900

    select, insert, login

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mod_dbconn
 
 app = Flask(__name__)
@@ -8,6 +8,13 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/insert')
+def insert_info():
+    return render_template('insert.html')
+
+@app.route('/login')
+def login():
+   return render_template('login.html') 
 
 # 여기부터 추가
 @app.route('/db')
@@ -20,6 +27,48 @@ def select():
     print(row)
 
     return render_template('db.html', resultData=row[0])
+
+
+
+@app.route('/db/insert', methods=['GET', 'POST'])
+def insert():
+    db_class = mod_dbconn.Database()
+    
+    if request.method == 'POST':
+        id = request.form['id']
+        pw = request.form['pw']
+        name = request.form['name']
+        phone = request.form['phone']
+        sql= "INSERT INTO test.test_tb VALUES({},{},{},{})".format(id, pw, phone, name)
+
+        db_class.execute(sql)
+        db_class.commit()
+
+        return render_template('home.html')
+   
+    else:
+        return "<h1>Fail</h1>"
+
+
+@app.route('/db/login', methods=['GET', 'POST'])
+def update():
+    db_class = mod_dbconn.Database()
+
+    if request.method == 'POST':
+        id = request.form['id']
+        pw = request.form['pw']
+
+        sql = "SELECT * FROM test.test_tb WHERE id = '{}' AND pw = '{}'".format(id, pw)
+
+        row = db_class.executeAll(sql)        
+        
+        print(row)
+
+        if len(row) != 0:
+            return render_template('home.html')
+        else:
+            return "<h1>LOGIN FAIL<h2>"
+
 
 # 여기까지 추가
 
